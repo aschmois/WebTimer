418e9072de2f4fc965256fa69efe528e1e45a62d
Andres Schmois <andresdroid@gmail.com>
2017-04-11 15:23:09 -0400

    Fix broken schedule

--- src/main/java/com/android305/lights/util/schedule/TimerScheduler.java
+++ src/main/java/com/android305/lights/util/schedule/TimerScheduler.java
@@ -23,18 +23,8 @@ public class TimerScheduler {
 
     public TimerScheduler() {
         try {
-            ZoneId z = ZoneId.systemDefault();
-            LocalDate today = LocalDate.now(z);
-            LocalDateTime tomorrowMidnight = LocalDateTime.of(today, LocalTime.MIDNIGHT).plusDays(1);
             sched = new StdSchedulerFactory().getScheduler();
             sched.start();
-            JobDetail job = newJob(DailyTask.class).withIdentity("dailyTask").usingJobData(DailyTask.FIRST_TIME, false).build();
-            Trigger trigger = newTrigger().withIdentity("dailyTaskTrigger")
-                                          .startAt(Date.from(tomorrowMidnight.atZone(z).toInstant()))
-                                          .withSchedule(simpleSchedule().withIntervalInHours(24).repeatForever())
-                                          .build();
-            sched.scheduleJob(job, trigger);
-            Log.d("Scheduled Daily Task at: " + tomorrowMidnight.toString());
             refreshTimerNow();
         } catch (SchedulerException e) {
             Log.e(e);
@@ -45,6 +35,16 @@ public class TimerScheduler {
     public void refreshTimerNow() throws SchedulerException {
         sched.clear();
         sched.scheduleJob(newJob(DailyTask.class).usingJobData(DailyTask.FIRST_TIME, true).build(), newTrigger().startNow().build());
+        ZoneId z = ZoneId.systemDefault();
+        LocalDate today = LocalDate.now(z);
+        LocalDateTime tomorrowMidnight = LocalDateTime.of(today, LocalTime.MIDNIGHT).plusDays(1);
+        JobDetail job = newJob(DailyTask.class).withIdentity("dailyTask").usingJobData(DailyTask.FIRST_TIME, false).build();
+        Trigger trigger = newTrigger().withIdentity("dailyTaskTrigger")
+                                      .startAt(Date.from(tomorrowMidnight.atZone(z).toInstant()))
+                                      .withSchedule(simpleSchedule().withIntervalInHours(24).repeatForever())
+                                      .build();
+        sched.scheduleJob(job, trigger);
+        Log.d("Scheduled Daily Task at: " + tomorrowMidnight.toString());
     }
 
     public void stop() {
